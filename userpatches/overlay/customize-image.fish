c1b99ca8558af9c77b555d2156ce65545ca2f844
CelestialCrafter <celestialcraftermc@gmail.com>
2025-05-11 06:38:51 -0500

    users && switch to fish

---
+++ userpatches/overlay/customize-image.fish
@@ -0,0 +1,52 @@
+set -g SETTINGS_PATH "/tmp/overlay/settings.json"
+
+# users
+useradd -s /usr/bin/fish -G sudo celestial
+usermod -s /usr/bin/fish root
+
+for user in string split "\0" (jq -r '.passwords | to_entries | map("\(.key)\0\(.value)") | .[]' $SETTINGS_PATH)
+	set -l name user[1]
+	set -l password user[2]
+
+	usermod -p $password $name
+end
+
+# wifi
+INTERFACE=(jq -r '.wifi.interface' $SETTINGS_PATH)
+awk \
+    -v iface=$INTERFACE \
+    -v ssid="$(jq -r '.wifi.ssid' $SETTINGS_PATH)" \
+    -v pw="$(jq -r '.wifi.password' $SETTINGS_PATH)" \
+    '{gsub(/SSID/, ssid); gsub(/PASSWORD/, pw); gsub(/INTERFACE/, iface)}1' \
+    /tmp/overlay/wifi.yaml > /etc/netplan/20-wifi.yaml
+chmod 600 /etc/netplan/20-wifi.yaml
+
+mkdir -p /etc/systemd/system/systemd-networkd-wait-online.service.d
+echo "[Service]
+ExecStart=
+ExecStart=/usr/lib/systemd/systemd-networkd-wait-online --timeout 30 -i $INTERFACE" > \
+    /etc/systemd/system/systemd-networkd-wait-online.service.d/override.conf
+
+# armbian stuff
+rm /etc/update-motd.d/*
+rm /etc/profile.d/armbian-*
+
+systemctl disable apt-daily.timer
+systemctl disable apt-daily-upgrade.timer
+systemctl disable dpkg-db-backup.timer
+systemctl disable man-db.timer
+
+# misc
+armbian-add-overlay /tmp/overlay/fan-control.dts
+echo "user_overlays=fan-control" >> /boot/armbianEnv.txt
+
+echo "celestial-homelab" > /etc/hostname
+
+# containers
+mkdir -p /etc/containers/systemd/
+cp /tmp/overlay/containers/* /etc/containers/systemd/
+
+awk -v key="$(jq -r '.soft_serve_admin_key' $SETTINGS_PATH)" \
+    '{gsub(/SS_ADMIN_KEY/, key)}1' \
+    /tmp/overlay/containers/soft-serve.container > /etc/containers/systemd/soft-serve.container
+
